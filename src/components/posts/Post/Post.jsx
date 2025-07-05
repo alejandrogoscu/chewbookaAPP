6c52f8c810eee2c92ebe2684366924535c48750b
alejandrogoscu <alejandrogoscu@gmail.com>
2025-07-05 21:25:11 +0200

    fixed getallposts

--- src/components/posts/Post/Post.jsx
+++ src/components/posts/Post/Post.jsx
@@ -1,20 +1,24 @@
 import { useSelector } from 'react-redux';
 
 const Post = () => {
-  const postsState = useSelector((state) => state.posts.posts);
-  const posts = postsState.posts;
+  const dataPosts = useSelector((state) => state.posts.posts);
+  const posts = dataPosts?.posts;
 
-  console.log(posts);
-  const post = posts.map((post) => {
-    return (
-      <div>
-        <h2>{post.title}</h2>
-        <p>{post.content}</p>
-        <img src={post.images}></img>
-      </div>
-    );
-  });
+  if (!Array.isArray(posts)) {
+    return <p>Cargando posts...</p>;
+  }
 
-  return <div>{post}</div>;
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post._id}>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+          <img src={post.images} />
+        </div>
+      ))}
+    </div>
+  );
 };
+
 export default Post;
